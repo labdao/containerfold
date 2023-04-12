7fd21fa464a09d579e2ebecd548c043c1c023464
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2023-04-12 13:58:43 +0200

    test

--- protbert/app.py
+++ protbert/app.py
@@ -19,23 +19,34 @@ def write_fasta_file(fasta_file: str, header: str, sequence: str):
         f.write(f"{header}\n{sequence}\n")
 
 @app.command()
+import torch
+
 def process_protein_sequence(
     input_sequence: Path = typer.Argument(..., help="Path to the protein sequence in FASTA format"),
     output_sequence: Path = typer.Argument(..., help="Path to the output FASTA file with the predicted sequence")
 ):
     if input_sequence.is_file():
         original_sequence = read_fasta_file(input_sequence)
-        
+
         tokenizer = AutoTokenizer.from_pretrained("Rostlab/prot_bert_bfd")
         model = AutoModelForMaskedLM.from_pretrained("Rostlab/prot_bert_bfd")
-        
+
         tokens = tokenizer(original_sequence, return_tensors="pt", padding=True, truncation=True)
         prediction = model(**tokens)
-        
-        # Process the prediction result as needed
-        # For demonstration purposes, let's assume the predicted sequence is the original sequence reversed
-        predicted_sequence = original_sequence[::-1]
-        
+
+        logits = prediction.logits
+        predicted_indices = torch.argmax(logits, dim=-1).squeeze().tolist()
+
+        # Replace 'X' symbols in the original sequence with the predicted amino acids
+        predicted_sequence = ""
+        token_sequence = tokenizer.tokenize(original_sequence)
+        for i, token in enumerate(token_sequence):
+            if token == "X":
+                predicted_aa = tokenizer.convert_ids_to_tokens([predicted_indices[i]])[0]
+                predicted_sequence += predicted_aa
+            else:
+                predicted_sequence += token
+
         # Write the predicted sequence to the output FASTA file
         output_header = ">predicted_sequence"
         write_fasta_file(output_sequence, output_header, predicted_sequence)
@@ -44,6 +55,7 @@ def process_protein_sequence(
     else:
         typer.echo(f"File not found: {input_sequence}")
         raise typer.Exit(code=1)
+
 
 if __name__ == "__main__":
     app()
