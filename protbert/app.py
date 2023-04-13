34dffe4c1eee39efdd2d1b32360a303d4d533a42
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2023-04-13 18:32:02 +0200

    fix spacing bug for mask

--- protbert/app.py
+++ protbert/app.py
@@ -17,19 +17,27 @@ def softmax(arr):
 
 def generate_embedding(record, tokenizer, bert_model, output):
     sequence = re.sub(r"[UZOB]", "X", str(record.seq))
-    encoded_input = tokenizer(sequence, return_tensors='pt')
+    spaced_sequence = " ".join(sequence)
+    print("embedding sequence: ", spaced_sequence)
+    encoded_input = tokenizer(spaced_sequence, return_tensors='pt')
     bert_output = bert_model(**encoded_input)
     embeddings = bert_output.last_hidden_state.detach().numpy()
     np.savetxt(os.path.join(output, f"{record.id}_encoded.csv"), embeddings[0], delimiter=",")
 
-def fill_mask(sequence, unmasker, output):
-    predictions = unmasker(sequence)
+def fill_mask(record, unmasker, output):
+    sequence = str(record.seq)
+    spaced_sequence = " ".join(sequence)
+    spaced_masked_sequence = spaced_sequence.replace("X", "[MASK]")
+    print("filling masked sequence: ", spaced_masked_sequence)
+    predictions = unmasker(spaced_masked_sequence)
     
-    with open(os.path.join(output, "predictions.json"), "w") as f:
+    with open(os.path.join(output, f"{record.id}_filled_mask.json"), "w") as f:
         json.dump(predictions, f)
 
-def generate_scoring_matrix(sequence, unmasker):
-    predictions = unmasker(sequence)
+def generate_scoring_matrix(record, unmasker):
+    sequence = str(record.seq).replace("X", "[MASK]")
+    spaced_sequence = " ".join(sequence)
+    predictions = unmasker(spaced_sequence)
 
     # Initialize an empty DataFrame to store scores
     scored = pd.DataFrame()
@@ -64,26 +72,25 @@ def main(input: str, output: str, mode: str, k: int = 10, n: int = 10):
         os.makedirs(output)
     
     # Load models and tokenizer
-    tokenizer = BertTokenizer.from_pretrained("Rostlab/prot_bert", do_lower_case=False)
+    tokenizer = BertTokenizer.from_pretrained("Rostlab/prot_bert", do_lower_case=True)
     masked_model = BertForMaskedLM.from_pretrained("Rostlab/prot_bert")
     bert_model = BertModel.from_pretrained("Rostlab/prot_bert")
     unmasker = pipeline('fill-mask', model=masked_model, tokenizer=tokenizer)
 
     # Load input sequence
     record = SeqIO.read(input, "fasta")
-    sequence = str(record.seq).replace("X", "[MASK]")
 
     if mode == "embedding":
         generate_embedding(record, tokenizer, bert_model, output)
     elif mode == "fill-mask":
-        fill_mask(sequence, unmasker, output)
+        fill_mask(record, unmasker, output)
         # Save the result to a file or print it as needed
     elif mode == "scoring-matrix":
-        generate_scoring_matrix(sequence, unmasker, output)
+        generate_scoring_matrix(record, unmasker, output)
     elif mode == "top-k":
-        top_k(sequence, unmasker, k, output)
+        top_k(record, unmasker, k, output)
     elif mode == "sample-n":
-        sample_n(sequence, unmasker, n, output)
+        sample_n(record, unmasker, n, output)
     else:
         typer.echo("Invalid mode. Please choose from 'embedding', 'fill-mask', 'scoring-matrix', 'top-k', or 'sample-n'.")
 
