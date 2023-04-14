5e99633283149dd9277e896429756eb97aa74e2e
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2023-04-14 17:47:55 +0200

    clean up toolbox and build ecr manager

--- installer.sh
+++ installer.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# build container
-# docker build containerfold -t containerfold:blank
-# docker build protbert -t protbert --no-cache
-
 
 aws_configure_from_env() {
     # Check if .env file exists
@@ -28,11 +24,54 @@ aws_configure_from_env() {
     fi
 }
 
+login_ecr() {
+    # Log in to ECR Public using AWS CLI
+    aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+    if [ $? -eq 0 ]; then
+        echo "Successfully logged in to ECR Public"
+    else
+        echo "Failed to log in to ECR Public"
+        return 1
+    fi
+}
+
+manage_ecr_repositories() {
+    # 1) Check the public ECR for the available repositories
+    ecr_repositories=$(aws ecr-public describe-repositories --query 'repositories[].repositoryName' --output text)
+    echo "ECR Public repositories: $ecr_repositories"
+
+    # 2) Check the available directories for Dockerfiles and list all LABEL container=XYZ
+    dockerfiles_with_label=$(grep -r "LABEL container=" . --include="Dockerfile" | awk -F'[=:]' '{print $3}')
+    echo "Dockerfiles found: $dockerfiles_with_label"
+
+    # 3) Create a new repository called XYZ if the repository does not yet exist in the public ECR
+    for label in $dockerfiles_with_label; do
+        repository_name=$(echo "$label" | tr -d '[:space:]') # Remove whitespace
+        if [[ ! $ecr_repositories =~ $repository_name ]]; then
+            echo "Creating new ECR Public repository: $repository_name"
+            aws ecr-public create-repository --repository-name "$repository_name"
+        else
+            echo "Repository $repository_name already exists"
+        fi
+    done
+}
+
+# Call the function
+
+
 # Call the function
 aws_configure_from_env
+login_ecr
+manage_ecr_repositories
 
+## TODO - add MoLeR
+
+## TODO remove
 # Add other functions and commands below
 
+# build container
+# docker build containerfold -t containerfold:blank
+# docker build protbert -t protbert --no-cache
 
 # check if the model weights exist in the container directory
 #if [ ! -d containerfold/params ]; then
